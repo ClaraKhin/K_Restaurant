636cc4939badbada2645ee17a5438ffab34e7902
Khin Thiri Kyaw <khinthirikyaw8@gmail.com>
2026-01-16 21:24:24 +0630

    feat : API creation Auth API’s Created User Model

---
+++ Restaurant_Backend/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => {
+                return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+            },
+            message: 'Please enter a valid email address!'
+        }
+    },
+    phone: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: (value) => {
+                return /^[0-9]{10}$/.test(value);
+            },
+            message: 'Please enter a valid phone number!'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: 0,
+    }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema); 
