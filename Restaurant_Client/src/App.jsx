f4b8b3a1076345482079173c9acf6d93eabda052
Khin Thiri Kyaw <khinthirikyaw8@gmail.com>
2026-01-18 10:18:40 +0630

    feat: implemented Auth Page

--- Restaurant_Client/src/App.jsx
+++ Restaurant_Client/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Orders from "./pages/Orders";
 import Tables from "./pages/Tables";
@@ -7,21 +12,30 @@ import Menu from "./pages/Menu";
 import Auth from "./pages/Auth";
 import Header from "./components/shared/Header";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+  const hideHeader = ["/auth"];
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/tables" element={<Tables />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-      </Router>
+      {!hideHeader.includes(location.pathname) && <Header />}
+      {/* hide header on auth page */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
   );
 }
 
