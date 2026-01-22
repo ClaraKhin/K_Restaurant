752d5d6045fc5ec4952eafa6660952ecee8076ea
Khin Thiri Kyaw <khinthirikyaw8@gmail.com>
2026-01-22 23:46:51 +0630

    feat : Add RecentOrders component with updated orders processing Implemented RecentOrders table with dynamic status dropdown Integrated hover effect for action button using useState Styled table and cells for better readability Updated orders data structure in constants

--- Restaurant_Client/src/constants/index.js
+++ Restaurant_Client/src/constants/index.js
@@ -211,7 +211,7 @@ export const menus = [
     { id: 6, name: "Pizzas", bgColor: "#0a3468ff", icon: "🍕", items: pizzas },
     { id: 7, name: "Alcholic Drinks", bgColor: "#423434ff", icon: "🍺", items: alcoholicDrinks },
     { id: 8, name: "Salads", bgColor: "#035220ff", icon: "🥗", items: salads },
-];
+]
 
 export const metricsData = [
     { title: "Revenue", value: "₹50,846.90", percentage: "12%", color: "#025cca", isIncrease: false },
@@ -226,3 +226,43 @@ export const itemsData = [
     { title: "Active Orders", value: "12", percentage: "12%", color: "#735f32", isIncrease: true },
     { title: "Total Tables", value: "10", color: "#7f167f" }
 ];
+
+export const orders = [
+    {
+        id: "101",
+        customer: "Amrit Raj",
+        status: "Ready",
+        dateTime: "January 18, 2025 08:32 PM",
+        items: 8,
+        tableNo: 3,
+        total: 250.0,
+    },
+    {
+        id: "102",
+        customer: "John Doe",
+        status: "In Progress",
+        dateTime: "January 18, 2025 08:45 PM",
+        items: 5,
+        tableNo: 4,
+        total: 180.0,
+    },
+    {
+        id: "103",
+        customer: "Emma Smith",
+        status: "Ready",
+        dateTime: "January 18, 2025 09:00 PM",
+        items: 3,
+        tableNo: 5,
+        total: 120.0,
+    },
+    {
+        id: "104",
+        customer: "Chris Brown",
+        status: "In Progress",
+        dateTime: "January 18, 2025 09:15 PM",
+        items: 6,
+        tableNo: 6,
+        total: 220.0,
+    },
+];
+
